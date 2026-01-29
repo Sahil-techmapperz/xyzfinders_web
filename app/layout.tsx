39fd73abb13e1afae50494b1a8b3163fad3d8061
Sahil90 <sahil.laskar@techmapperz.com>
2026-01-29 13:24:52 +0530

    Configure favicons: Add icons to public and app, update layout metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,6 +20,10 @@ export default function RootLayout({
           href="https://cdn.jsdelivr.net/npm/remixicon@4.3.0/fonts/remixicon.css"
           rel="stylesheet"
         />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="manifest" href="/site.webmanifest" />
       </head>
       <body className="antialiased">
         <MainLayout>{children}</MainLayout>
